eeed0d8ebcc878d04c3a9ac1934a3741a0ff49fc
dilrong <dilrong@dilrong.com>
2023-09-08 22:46:42 +0900

    build: GA test

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -11,12 +11,13 @@ const Analytics = () => {
       />
       <Script id="google-analytics">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');
-        `}
+    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');
+
+    `}
       </Script>
       <VercelAnalytics />
     </>
