392e05a339de7cc8fff24a20568356f154f0bcc1
dilrong <dilrong@dilrong.com>
2023-09-10 17:28:08 +0900

    build: nextjs-google-analytics

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,34 +1,12 @@
 "use client";
 
-import Script from "next/script";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
 
 const Analytics = () => {
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('consent', 'default', {
-                    'analytics_storage': 'denied'
-                });
-                
-                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}', {
-                    page_path: window.location.pathname,
-                });
-                `,
-        }}
-      />
+      <GoogleAnalytics trackPageViews />
       <VercelAnalytics />
     </>
   );
