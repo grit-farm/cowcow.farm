d147d9b1b2c97601a27f7c0f3955b3af02634ddc
dilrong <dilrong@dilrong.com>
2023-09-12 08:06:19 +0900

    feat: GA test

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,13 +1,22 @@
 "use client";
 
-import { GoogleAnalytics } from "nextjs-google-analytics";
-import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const Analytics = () => {
   return (
     <>
-      <GoogleAnalytics trackPageViews />
-      <VercelAnalytics />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');
+        `}
+      </Script>
     </>
   );
 };
